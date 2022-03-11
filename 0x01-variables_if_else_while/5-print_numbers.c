0e5cb4ad8b1ccd2997a359a92a1cbc736fd79096
Emmanuel Effiong <etokoffice@gmail.com>
2022-03-11 15:17:13 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry block
+ * Description: print single digits less than 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
