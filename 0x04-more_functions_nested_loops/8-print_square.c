020c64802433d100cc6fbee6ce8781003d611f92
Emmanuel Effiong <etokoffice@gmail.com>
2022-03-17 14:51:48 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,23 +5,19 @@
  * @size: dimensions of the sqaure
  */
 
-void print_sqaure(int size)
+void print_square(int size)
 {
-	int i, j;
+	int i;
+	int j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	for (j = 0; j < size; j++)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
+	if (size <= 0)
+		_putchar('\n');
 }
