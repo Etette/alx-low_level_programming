88b6b1159f5b6798fbabe2baf5e81a9ba3105274
Emmanuel Effiong <etokoffice@gmail.com>
2022-03-17 14:45:20 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a sqaure of variable length
+ * @size: dimensions of the sqaure
+ */
+
+void print_sqaure(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
