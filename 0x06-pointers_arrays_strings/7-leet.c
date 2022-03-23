16b54584a857320eb7bc6da98e8f267e0c32cfdd
Emmanuel Effiong <etokoffice@gmail.com>
2022-03-23 15:42:48 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,17 +9,12 @@
 char *leet(char *s)
 {
 	int i, j;
-	char l[] = "ol_ea_t";
+	char l[] = "aeotlAEOTL";
+	char n[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		for (j = 0; l[j] != '\0'; j++)
-		{
-			if (s[i] == l[j] || s[i] == (l[j] - 32))
-			{
-				s[i] = j + '0';
-			}
-		}
-	}
+			if (s[i] == l[j])
+				s[i] = n[j];
 	return (s);
 }
