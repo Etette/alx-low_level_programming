9daec2030fdf5285cfb823425a0c406cbe27aa2b
Emmanuel Effiong <etokoffice@gmail.com>
2022-03-30 07:49:22 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - outputs x raised to y power
+ * @x: int param
+ * @y: int param
+ * Return: x**y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		if (y == 0)
+			return (1);
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
+}
