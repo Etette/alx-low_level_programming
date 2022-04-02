c0fb863cd8e900d126eacfdbad77db3946aca024
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-02 14:57:50 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-/**
- * _isnumber - checks if string is a number
- * @s: char array
- * Return: 1
- */
-
-int _isnumber(char *s)
-{
-	int i, check, d;
-
-	i = 0, d = 0, check = 1;
-
-	if (*s == '-')
-		i++;
-	for (; *(s + 1) != 0; i++)
-	{
-		d = isdigit(*(s + 1));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
-	}
-	return (check);
-}
 
 /**
  * main - add positive numbers
@@ -37,23 +10,15 @@ int _isnumber(char *s)
  */
 int main(int argc, char **argv)
 {
-	int i, j, res;
-
-	res = 0, j = 0;
+	int i, j, res = 0;
 
 	if (argc > 1)
-	{
 		for (i = 1; i < argc; i++)
 		{
-			if (_isnumber(argv[i]))
-				j += atoi(argv[i]);
-			else
-				res = 1;
+			for (j = 0; argv[i][j] < '0' || argv[i][j] > '9'; j++)
+				return (printf("Error\"n"), 1);
+			res += atoi(argv[i]);
 		}
-	}
-	if (res == 0)
-		printf("%i\n", j);
-	else
-		printf("%s\n", "Error");
-	return (res);
+	printf("%i\n", res);
+	return (0);
 }
