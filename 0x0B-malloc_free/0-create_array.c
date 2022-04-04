83c3013ec3cd2d158a5a81d500d6af19e9838d6e
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-04 15:02:09 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(size * sizeof(c));
 
-	if (size == 0)
+	if (!size || !arr)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
