316a670394ba081eda7beb46a1bdff764f963210
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-13 14:44:36 -0700

    function_pointer.h

--- 0x0F-function_pointers/function_pointer.h
+++ 0x0F-function_pointers/function_pointer.h
@@ -1,5 +1,8 @@
-#ifndef _FUNCTION_POINTER_H
-#define _FUNCTION_POINTER_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
