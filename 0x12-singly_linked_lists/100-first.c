e57d34bac6274f3c1ae8c16761d56e1c8f9621da
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-21 23:10:37 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void theHare(void)__attribute__((constructor));
 
 void theHare(void)
 {
-	printf("You're beat and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
