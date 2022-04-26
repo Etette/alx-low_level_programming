7a9f70e133027cc9dd2af2ccda7c1682194e3fef
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-26 22:14:02 +0000

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to head node
+ * Return: address of node at start of loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	return (NULL);
+}
