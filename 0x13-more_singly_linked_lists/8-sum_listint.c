39656d810dd0f9a9cb7d0428d723811bf1ddcfb5
Emmanuel Effiong <etokoffice@gmail.com>
2022-04-26 22:25:47 +0000

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,19 +7,19 @@
  */
 int sum_listint(listint_t *head)
 {
-	listint_t *curr;
+	listint_t *current;
 
-	int sum = 0;
+	register int sum = 0;
 
 	if (head == NULL)
 		return (0);
 
-	curr = head;
+	current = head;
 
-	while (curr != NULL)
+	while (current != NULL)
 	{
-		sum += curr->n;
-		curr = curr->next;
+		sum += current->n;
+		current = current->next;
 	}
 
 	return (sum);
