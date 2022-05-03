ee58963f956fd05bf217c70445e497b2f4a5fd6a
Emmanuel Effiong <etokoffice@gmail.com>
2022-05-03 16:22:42 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,88 @@
 #include "main.h"
 #include <stdio.h>
 
-#define MAXSIZE 1204
-#define SE STDERR_FILENO
+#define BUFSIZE 1204
 
 /**
- * main - create the copy of bash script
- * @ac: arg count
- * @av: arg vector ( as string)
- * Return: 0
+ * file1fail - print error message if can' read file
+ * @file: name of the file
  */
 
-int main(int ac, char *av[])
+void file1fail(char *file)
 {
-	int input_fd, output_fd, istatus, ostatus;
-	char buf[MAXSIZE];
-	mode_t mode;
+	dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file);
+	exit(98);
+}
 
-	mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IROTH;
-	if (ac != 3)
-		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
-	input_fd = open(av[1], O_RDONLY);
-	if (input_fd == -1)
-		dprintf(SE, "Error: can't read from file %s\n", av[1]), exit(98);
-	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (output_fd == -1)
-		dprintf(SE, "Error: can't write to %s\n", av[2]), exit(99);
+/**
+ * file2fail - print error message if can;t write to file
+ * @file: name of the file
+ */
 
-	do {
-		istatus = read(input_fd, buf, MAXSIZE);
-		if (istatus == -1)
-		{
-			dprintf(SE, "Error: can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		if (istatus > 0)
-		{
-			ostatus = write(output_fd, buf, (ssize_t) istatus);
-			if (ostatus == -1)
-				dprintf(SE, "Error: can't write to %s\n", av[2]), exit(99);
-		}
-	} while (istatus > 0);
-	istatus = close(input_fd);
-	if (istatus == -1)
-		dprintf(SE, "Error: can't close fd %d\n", input_fd), exit(100);
-	ostatus = close(output_fd);
-	if (ostatus == -1)
-		dprintf(SE, "Error: can't close fd %d\n", output_fd), exit(100);
+void file2fail(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: can't write to %s\n", file);
+	exit(99);
+}
+
+/**
+ * closefail - print error message if file can't close
+ * @fd: file descriptor of the file
+ *
+ */
+
+void closefail(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * main - copy contents of one file to another
+ * @argc: number of args recieved
+ * @argv: array of args recieved
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int file1, file2, file1rd, file2wr, closed;
+	char buffer[BUFSIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (argv[1] == NULL)
+		file1fail(argv[1]);
+	if (argv[2] == NULL)
+		file2fail(argv[2]);
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+		file1fail(argv[1]);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (file2 == -1)
+		file2fail(argv[2]);
+	file1rd = read(file1, buffer, BUFSIZE);
+	if (file1rd == -1)
+		file1fail(argv[1]);
+	while (file1rd > 0)
+	{
+		file2wr = write(file2, buffer, file1rd);
+		if (file2wr != file1rd)
+			file2fail(argv[2]);
+		file1rd = read(file1, buffer, BUFSIZE);
+		if (file1rd == -1)
+			file1fail(argv[1]);
+	}
+	closed = close(file1);
+	if (closed == -1)
+		closefail(file1);
+	closed = close(file2);
+	if (closed == -1)
+		closefail(file2);
 	return (0);
 }
